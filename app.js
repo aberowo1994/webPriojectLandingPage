a561dcc5121e6a2afa6150b705356cfc86907585
jason oluwaseyifunmi <abere1994@gmail.com>
2021-08-25 23:45:34 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const hamburger = document.querySelector('.hamburger');
+
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-links-hidden');
+    // navLinks.style.transition= 'all 0.5s ease';
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let moon = document.querySelector('#moon');
+
+
+// moon.addEventListener('click', function(){
+//     document.body.classList.toggle('dark-theme');
+//     if (document.body.classList.contains('dark-theme')){
+//         moon.src = "images/sun1.png";
+//     } else {
+//         moon.src = "/images/moon2.png"
+//     }
+// })
